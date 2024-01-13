c9862e85ada3af625cfe44016050898b5d477262
Thorsten Wißmann <edu@thorsten-wissmann.de>
2024-01-13 23:31:09 +0100

    Upload agda html doc

--- make-html.sh
+++ make-html.sh
@@ -6,3 +6,10 @@ agda --html --html-dir=_html/ "index.agda"
 
 echo "open the doc in your browser: file://`pwd`/_html/index.html"
 
+if [[ "$1" = --install ]] ; then
+    rsync -rva _html/ uber:html/initial-algebras-unchained
+    cat <<EOF
+
+    ====>   https://thorsten-wissmann.de/initial-algebras-unchained/
+EOF
+fi
