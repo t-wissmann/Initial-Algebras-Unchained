30ec21b5e7667a29590353c0c594db7009700c61
Thorsten Wißmann <edu@thorsten-wissmann.de>
2024-01-15 10:28:42 +0100

    Private html doc url

--- make-html.sh
+++ make-html.sh
@@ -6,10 +6,12 @@ agda --html --html-dir=_html/ "index.agda"
 
 echo "open the doc in your browser: file://`pwd`/_html/index.html"
 
-if [[ "$1" = --install ]] ; then
-    rsync -rva _html/ uber:html/initial-algebras-unchained
-    cat <<EOF
+#    rsync -rva _html/ uber:html/initial-algebras-unchained
+#    ====>   https://thorsten-wissmann.de/initial-algebras-unchained/
 
-    ====>   https://thorsten-wissmann.de/initial-algebras-unchained/
+if [[ "$1" = --install ]] ; then
+    rsync -rva _html/ thorsten_unchained@ssh.nyc1.nearlyfreespeech.net:/home/public/iau-l24/
+    cat <<EOF
+    ====>   https://unchained.nfshost.com/iau-l24/
 EOF
 fi
